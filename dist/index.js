1f876db96101f3d056e7cfabb5a02233468e5728
Christian Brevik <christian.brevik@gmail.com>
2017-05-05 09:13:59 +0200

    Initial with webpack ++

---
+++ dist/index.js
@@ -0,0 +1,45 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var react_native_1 = require("react-native");
+var App = (function (_super) {
+    __extends(App, _super);
+    function App() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.state = { name: 'Nader' }; // 4
+        return _this;
+    }
+    App.prototype.render = function () {
+        return (react_1.default.createElement(react_native_1.View, { style: styles.container },
+            react_1.default.createElement(react_native_1.Text, { style: styles.text },
+                "Welcome ",
+                this.state.name,
+                "! // 5")));
+    };
+    return App;
+}(react_1.Component));
+exports.default = App;
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+//# sourceMappingURL=index.js.map
